798bf65aafdcda3904a4fd46f07ee44324a48e81
axle <axlevisu@gmail.com>
2017-04-01 23:21:17 +0530

    cnn code added

--- src/keras-numerical.py
+++ src/keras-numerical.py
@@ -1,9 +1,16 @@
 import numpy as np
+from imread import imread
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
-from imread import imread
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
 # from PIL import Image
 # fix random seed for reproducibility
 seed = 7
@@ -26,17 +33,29 @@ X_test = X[2400:]
 y_test = y[2400:]
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
+
+X_train = X_train.reshape(X_train.shape[0], 1, 32, 32).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 1, 32, 32).astype('float32')
+
 num_classes = y_test.shape[1]
 pixels = len(X[0])
 shape = X[0].shape
 model = Sequential()
-model.add(Dense(pixels, input_shape = shape, kernel_initializer='normal',  activation='relu'))
-model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
+model.add(Conv2D(20, (8, 8), input_shape=(1, 32, 32), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(15, (4, 4), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))	
+model.add(Dense(50, activation='relu'))
+# model.add(Dense(pixels, input_shape = shape, kernel_initializer='normal',  activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=20, batch_size=100)
 
 scores = model.evaluate(X_test, y_test, verbose=0)
-print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+print("CNN Error: %.2f%%" % (100-scores[1]*100))
 
 
