385262ed49d0ae06fe4e2d4e7e8a75616d9aa12f
axle <axlevisu@gmail.com>
2017-04-02 01:16:14 +0530

    character data added and using PIL Image library instead of imread

--- src/keras-numerical.py
+++ src/keras-numerical.py
@@ -1,5 +1,6 @@
 import numpy as np
-from imread import imread
+# from imread import imread
+from PIL import Image
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -11,7 +12,6 @@ from keras.utils import np_utils
 from keras import backend as K
 K.set_image_dim_ordering('th')
 
-# from PIL import Image
 # fix random seed for reproducibility
 seed = 7
 np.random.seed(seed)
@@ -20,8 +20,8 @@ X = []
 y =[]
 for i in xrange(3000):
 	y.append(i%10)
-	image = imread(numbers_dir +"img" + str(i+1)+'.bmp')
-	image = image[:,:,0].flatten()
+	image = Image.open(numbers_dir +"img" + str(i+1)+'.bmp')
+	image = np.array(image)[:,:,0].flatten()
 	X.append(np.array(image)/255)
 
 X = np.array(X)
@@ -47,13 +47,13 @@ model.add(Conv2D(15, (4, 4), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))	
-model.add(Dense(50, activation='relu'))
+model.add(Dense(150, activation='relu'))	
+model.add(Dense(60, activation='relu'))
 # model.add(Dense(pixels, input_shape = shape, kernel_initializer='normal',  activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=20, batch_size=100)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=100)
 
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("CNN Error: %.2f%%" % (100-scores[1]*100))
