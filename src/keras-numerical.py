608b589320d25777962b789fb42bd3515c981d1c
axle <axlevisu@gmail.com>
2017-04-01 21:22:10 +0530

    read image using imread module

--- src/keras-numerical.py
+++ src/keras-numerical.py
@@ -3,12 +3,20 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.utils import np_utils
-from PIL import Image
+from imread import imread
+# from PIL import Image
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
 numbers_dir = "../data/"
 X = []
 y =[]
-for i in xrange(1,3001):
-	bmpfile = 
+for i in xrange(3000):
+	y.append(1)
+	image = imread(numbers_dir + str(i+1)+'.bmp')
+	image = image[:,:,0].flatten()
+	X.append(np.array(X))
+
+
+
+
