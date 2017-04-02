1eef3c4596c37aa0539290a8a9cb6889000c6fd1
axle <axlevisu@gmail.com>
2017-04-02 09:11:01 +0530

    preprocessed data and max sized to 64pixels

--- src/keras-characters.py
+++ src/keras-characters.py
@@ -1,7 +1,7 @@
 # keras-characters
 import numpy as np
 # from imread import imread
-from PIL import Image
+from PIL import Image, ImageFilter
 import glob
 # from keras.models import Sequential
 # from keras.layers import Dense
@@ -18,19 +18,27 @@ import glob
 X = []
 y =[]
 # Restrict maximum width/height to 128 due to memory constraints
-max_size =128.0
+max_size =64.0
 char_dir = '../data/characters/'
 for i in xrange(147):
 	folder = char_dir + 'usr_' + str(i) + '/'
 	for imagefile in glob.glob(folder + '*.tiff'):
 		im = Image.open(imagefile)
-		try:
-			y.append(int(imagefile[-11:-8]))
-		except:
-			print imagefile
+		image_name = imagefile[-11:-8]
+		# Dilating Image before resize
+		im = im.filter(ImageFilter.MinFilter(7))
+		im = im.convert('1')
+
+		# try:
+		# 	y.append(int(image_name))
+		# except:
+		# 	print imagefile
+
+		# Resizing Image
 		f = max_size/max(im.size)
 		im = im.resize((int(round(f*im.size[0])),int(round((f*im.size[1])))),Image.ANTIALIAS)
-		X.append(np.array(im)*1)
+		im.save( imagefile[:-11] + '000' + imagefile[-11:-5] + 'r.tiff' )
+		# X.append(np.array(im)*1)
 		# max_size = max(max_size,max(im.size))
 
 print max_size
