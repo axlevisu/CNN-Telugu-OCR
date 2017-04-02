ac7c1c4646c161af59f11577582e79fb5bf7d090
axle <axlevisu@gmail.com>
2017-04-02 16:38:14 +0530

    working code, cnn

--- src/keras-characters.py
+++ src/keras-characters.py
@@ -16,20 +16,26 @@ K.set_image_dim_ordering('th')
 X = []
 y =[]
 # Restrict maximum width/height to 128 due to memory constraints
-max_size =64.0
+max_size =64
 char_dir = '../data/characters/'
-for i in xrange(147):
+for i in xrange(21):
 	folder = char_dir + 'usr_' + str(i) + '/'
 	for imagefile in glob.glob(folder + '*r.tiff'):
 		im = Image.open(imagefile)
 		image_name = imagefile[-12:-9]
 
-		y.append(int(image_name))		
-		X.append(np.array(im)*1)
+		y.append(int(image_name))
+		# Padding image to make it 64x64
+		im = np.array(im)*1
+		b = np.ones((max_size, max_size))
+		b[:im.shape[0],:im.shape[1]] = im 		
+		X.append(b)
 
 t = int(round(len(X)*0.8))		
 X = np.array(X)
 y = np.array(y)
+X = X.reshape(X.shape[0], 1, max_size, max_size).astype('float32')
+
 X_train = X[0:t]
 y_train = y[0:t]
 
@@ -38,10 +44,10 @@ y_test = y[t:]
 # Convert to vectors
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
-
+num_classes = y_test.shape[1]
 model = Sequential()
 # model.add(ZeroPadding2D((1,1),input_shape=(1,None,None)))
-model.add(Conv2D(50, (5, 5), input_shape=(1,None,None), activation='relu'))
+model.add(Conv2D(50, (5, 5), input_shape=(1,max_size,max_size), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(20, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
