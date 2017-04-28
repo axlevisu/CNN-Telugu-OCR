c4b16a8a747729e7ffcfdd9380a6813df7ac620b
sasuke <santhukota1996@gmail.com>
2017-04-29 00:48:53 +0530

    keras-numerical changed

--- src/keras-numerical.py
+++ src/keras-numerical.py
@@ -25,10 +25,14 @@ for i in xrange(3000):
 
 X = np.array(X)
 y = np.array(y)
+Z = np.c_[X,y]
+np.random.shuffle(Z)
+X = Z[:,:-1]
+y = Z[:,-1]
 shape = X[0].shape
 X = X.reshape(X.shape[0], 1, 32, 32).astype('float32')
 
-
+print "HI"
 X_train = X[0:2400]
 y_train = y[0:2400]
 
@@ -56,7 +60,7 @@ model.add(Dense(50, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=100)
+model.fit(X_train, y_train, validation_split=0.25, epochs=10, batch_size=100)
 
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("CNN Error: %.2f%%" % (100-scores[1]*100))
